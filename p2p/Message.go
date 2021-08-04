2671bf3bb749aa12d045ad97a07bd56375d2468a
Manfred Stoiber <manfred.stoiber@stud.hs-coburg.de>
2021-08-04 21:16:51 +0200

    [Stoiber] Added nonce, created structs for the various message types

--- p2p/Message.go
+++ p2p/Message.go
@@ -8,6 +8,14 @@ import (
 	"strings"
 )
 
+const KDM_PING uint16 = 654
+const KDM_PONG uint16 = 655
+const KDM_STORE uint16 = 656
+const KDM_FIND_NODE uint16 = 657
+const KDM_FIND_NODE_ANSWER uint16 = 658
+const KDM_FIND_VALUE uint16 = 659
+const KDM_FIND_VALUE_ANSWER uint16 = 660
+
 type message struct {
 	sender   peer
 	receiver peer
@@ -15,27 +23,56 @@ type message struct {
 	// header
 	size        uint16
 	messageType uint16
+	nonce       id
 	responseTo  uint32
 
 	// body
 	data []byte
 }
 
-func readMessage(conn net.Conn, message *message) {
+type message_ping struct {
+	message
+}
 
-	//   get message size
+type message_pong struct {
+	message
+}
+
+type message_find_node struct {
+	message
+	key id
+}
+
+func readMessage(conn net.Conn) {
+
+	var messageHeader message
+
+	//   get messageHeader size
 	messageSize := make([]byte, 2)
 	conn.Read(messageSize)
-	message.size = binary.BigEndian.Uint16(messageSize)
+	messageHeader.size = binary.BigEndian.Uint16(messageSize)
 
-	messageData := make([]byte, 0, message.size)
+	messageData := make([]byte, 0, messageHeader.size)
 	messageData = append(messageData, messageSize...)
 	conn.Read(messageData[2:])
-	message.data = messageData
-	message.messageType = binary.BigEndian.Uint16(messageData[2:4])
 
-	message.sender = parseByteToPeer(messageData[4:44])
+	//messageHeader.data = messageData
+	messageHeader.messageType = binary.BigEndian.Uint16(messageData[2:4])
+	messageHeader.sender = parseByteToPeer(messageData[4:42])
+	copy(messageHeader.nonce[:], messageData[42:62])
 
+	switch messageHeader.messageType {
+	case KDM_PING:
+		var message = message_ping{}
+		message.message = messageHeader
+	case KDM_PONG:
+		var message = message_pong{}
+		message.message = messageHeader
+	case KDM_FIND_NODE:
+		var message = message_find_node{}
+		message.message = messageHeader
+		copy(message.key[:], messageData[62:82])
+	}
 }
 
 //sends the data of message m to the receiver in message m
@@ -140,7 +177,7 @@ func parseByteToPeer(data []byte) peer {
 	p := peer{
 		id:   id,
 		ip:   string(data[20:36]),
-		port: binary.BigEndian.Uint32(data[36:]),
+		port: binary.BigEndian.Uint16(data[36:]),
 	}
 	return p
 }
